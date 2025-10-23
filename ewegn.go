5dc5b22de94fc59405d86861064d19077eed1c04
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-10-22 21:47:45 -0500

    Word list init

--- ewegn.go
+++ ewegn.go
@@ -1,25 +1,78 @@
 ﻿package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 const (
-	squareUnknown  = ":black_large_square:"
-	squareAbsent   = ":black_large_square:"
-	squareMaybe    = ":yellow_square:"
-	squarePresent  = ":green_square:"
-	squareNoGuess  = ":blue_square:"
-	zeroWidthSpace = "\u200b"
+	squareUnknown = ":black_large_square:"
+	squareAbsent  = ":black_large_square:"
+	squareMaybe   = ":yellow_square:"
+	squarePresent = ":green_square:"
+	squareNoGuess = ":blue_square:"
+
+	validAnswersFile = "valid_answers.txt"
+	validGuessesFile = "valid_guesses.txt"
+
+	// Based on the number of lines in each corresponding file
+	answersCount = 2315
+	guessesCount = 10657
 )
 
 var (
-	Sessions = make(map[string]*EwegnSession)
+	Sessions     = make(map[string]*EwegnSession)
+	ValidWords   = make(map[string]struct{}, answersCount+guessesCount)
+	ValidAnswers = make([]string, 0, answersCount)
 )
+
+func init() {
+	answersFile, err := os.Open(validAnswersFile)
+	if err != nil {
+		fmt.Println("ERROR: Failed to open answers file!", err)
+		panic(err)
+	}
+	defer answersFile.Close()
+
+	answersScanner := bufio.NewScanner(answersFile)
+	for answersScanner.Scan() {
+		ValidAnswers = append(ValidAnswers, answersScanner.Text())
+		ValidWords[answersScanner.Text()] = struct{}{}
+	}
+
+	err = answersScanner.Err()
+	if err != nil {
+		fmt.Println("ERROR: Failed to read answers file!", err)
+		panic(err)
+	}
+
+	guessesFile, err := os.Open(validGuessesFile)
+	if err != nil {
+		fmt.Println("ERROR: Failed to open guesses file!", err)
+		panic(err)
+	}
+	defer guessesFile.Close()
+
+	guessesScanner := bufio.NewScanner(guessesFile)
+	for guessesScanner.Scan() {
+		ValidWords[guessesScanner.Text()] = struct{}{}
+	}
+
+	err = guessesScanner.Err()
+	if err != nil {
+		fmt.Println("ERROR: Failed to read guesses file!", err)
+		panic(err)
+	}
+}
 
 type EwegnSession struct {
 	Owner       string     // ID of the user who is playing this session
 	RevealBoard [6][5]byte // The colors of spaces that have been guessed
 	GuessBoard  [6][5]byte // The letters guessed
 	RoundNumber byte       // The current round number (0 to 5)
+	Answer      string     // The correct answer for this session
 }
 
 const (
@@ -62,9 +115,6 @@ func (s *EwegnSession) ToString() string {
 			str.WriteString(":regional_indicator_")
 			str.WriteByte(c)
 			str.WriteByte(':')
-			// Discord will concatenate regional indicator emotes into country flags if they adjacent. Adding a
-			// zero-width space after each of them will prevent this.
-			str.WriteString(zeroWidthSpace)
 		}
 
 		str.WriteByte('\n')
