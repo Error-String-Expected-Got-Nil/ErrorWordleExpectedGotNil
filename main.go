de8ee97d450fa670315141d09d87709091bdfdf7
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-10-22 19:25:14 -0500

    Hypothetically basically functional
    But for some reason I get an authentication error only when I read the token from a file. If I hardcode it, it works fine. I have no clue why this is.

--- main.go
+++ main.go
@@ -2,8 +2,72 @@
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const (
+	tokenFile = "token"
+	prefix    = "::"
 )
 
 func main() {
-	fmt.Println("Test")
+
+	data, err := os.ReadFile(tokenFile)
+	if err != nil {
+		fmt.Printf("Failed to read bot token: %s\n", err.Error())
+		return
+	}
+
+	token := string(data)
+
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Printf("Failed to initialize bot: %s\n", err.Error())
+		return
+	}
+
+	dg.AddHandler(onMessageCreate)
+	
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Printf("Failed to open connection to Discord: %s\n", err.Error())
+		return
+	}
+
+	fmt.Println("EWEGN bot has started up, press CTRL-C to terminate.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	_ = dg.Close()
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.Bot {
+		return
+	}
+
+	if !strings.HasPrefix(m.Content, prefix) {
+		return
+	}
+
+	args := strings.Fields(strings.TrimPrefix(m.Content, prefix))
+
+	if len(args) < 1 {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "No command given.")
+		return
+	}
+
+	switch args[0] {
+	case "test":
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Test response")
+	}
 }
